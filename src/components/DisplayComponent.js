b8d51c1361470dd8d4a56359442e0c2b9cf9b05b
KarinaDavtyan <k.davtyan27@gmail.com>
2018-03-31 18:35:11 +0200

    feat(displayComponent): show text from redux state

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 class DisplayComponent extends React.Component {
 
+  renderWords = () => {
+    const words = this.props.text.map(word => {
+      return (
+        <div className='Word' key={word}>
+          {word}
+        </div>
+      )
+    })
+    return words;
+  }
   render () {
     return (
       <div className='DisplayComponent'>
+        <div className='WordsList'>
+          {this.props.text && this.renderWords()}
+        </div>
       </div>
     )
   }
--- src/containers/PhoneContainer.js
+++ src/containers/PhoneContainer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearText } from '../actions';
+
 import DisplayComponent from '../components/DisplayComponent';
 import ControlButtonsComponent from '../components/ControlButtonsComponent';
 import NumButtonsComponent from '../components/NumButtonsComponent';
@@ -8,12 +12,18 @@ class PhoneContainer extends React.Component {
   render () {
     return (
       <div className='PhoneContainer'>
-        <DisplayComponent />
-        <ControlButtonsComponent />
+        <DisplayComponent text={this.props.text} />
+        <ControlButtonsComponent clearText={this.props.clearText}/>
         <NumButtonsComponent />
       </div>
     )
   }
 }
 
-export default PhoneContainer;
+const mapStateToProps = (state) => ({
+  text: state.text
+})
+
+export default connect(mapStateToProps,
+  { clearText }
+)(PhoneContainer);
