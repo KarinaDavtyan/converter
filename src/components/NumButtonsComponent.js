5a024b7e479f6d55732fb2d67e7d4f2f0247bbbc
KarinaDavtyan <k.davtyan27@gmail.com>
2018-03-31 18:32:00 +0200

    refactor(numButtons): render buttons from keyboard object

--- src/components/NumButtonsComponent.js
+++ src/components/NumButtonsComponent.js
@@ -1,88 +1,60 @@
 import React from 'react';
 
-const NumButtonsComponent = () => {
-  return (
-    <div className='NumButtons'>
-      <div className='FirstRowButtons'>
-        <button>
+class NumButtonsComponent extends React.Component {
+
+  renderButtons = () => {
+    const keyboard = {
+      1: [
+        ''
+      ],
+      2: [
+        'a','b','c'
+      ],
+      3: [
+        'd','e','f'
+      ],
+      4: [
+        'g','h','i'
+      ],
+      5: [
+        'j','k','l'
+      ],
+      6: [
+        'm','n','o'
+      ],
+      7: [
+        'p','q','r','s'
+      ],
+      8: [
+        't','u','v'
+      ],
+      9: [
+        'w','x','y','z'
+      ]
+    }
+
+    const buttons = Object.keys(keyboard).map(key => {
+      return (
+        <button key={key}>
           <div className='KeyNumber'>
-            1
-          </div>
-          <div className='KeyLetters' style={{'fontSize': '2vh'}}>
-            &#9141;
-          </div>
-        </button>
-        <button>
-          <div className='KeyNumber'>
-            2
+            {key}
           </div>
           <div className='KeyLetters'>
-            ABC
+            {keyboard[key]}
           </div>
         </button>
-        <button>
-          <div className='KeyNumber'>
-            3
-          </div>
-          <div className='KeyLetters'>
-            DEF
-          </div>
-        </button>
+      )
+    })
+    return buttons;
+  }
+
+  render () {
+    return (
+      <div className='NumButtons'>
+        {this.renderButtons()}
       </div>
-      <div className='SecondRowButtons'>
-        <button>
-          <div className='KeyNumber'>
-            4
-          </div>
-          <div className='KeyLetters'>
-            GHI
-          </div>
-        </button>
-        <button>
-          <div className='KeyNumber'>
-            5
-          </div>
-          <div className='KeyLetters'>
-            JKL
-          </div>
-        </button>
-        <button>
-          <div className='KeyNumber'>
-            6
-          </div>
-          <div className='KeyLetters'>
-            MNO
-          </div>
-        </button>
-      </div>
-      <div className='ThirdRowButtons'>
-        <button>
-          <div className='KeyNumber'>
-            7
-          </div>
-          <div className='KeyLetters'>
-            PQRS
-          </div>
-        </button>
-        <button>
-          <div className='KeyNumber'>
-            8
-          </div>
-          <div className='KeyLetters'>
-            TUV
-          </div>
-        </button>
-        <button>
-          <div className='KeyNumber'>
-            9
-          </div>
-          <div className='KeyLetters'>
-            WXYZ
-          </div>
-        </button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default NumButtonsComponent;
